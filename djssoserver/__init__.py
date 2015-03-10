796bb5c316d61050fe34d7864308a0d27d79846b
FEI FAN <feifan.pub@gmail.com>
2015-03-10 11:56:27 -0700

    add __version__ in __init__.py

--- djssoserver/__init__.py
+++ djssoserver/__init__.py
@@ -7,3 +7,7 @@ def _load_setting(n, default):
     return getattr(settings, n) if hasattr(settings, n) else default
 
 SSO_SERVER_USER_MODEL_TO_DICT_CLS = _load_setting("SSO_SERVER_USER_MODEL_TO_DICT_CLS", "djssoserver.utility.DjangoJSONEncoder")
+
+
+__version__ = '0.5'
+VERSION = tuple(map(int, __version__.split('.')))
