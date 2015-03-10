11c7eaa458d753e5e6d4cea20e3e84ec0549f69d
FEI FAN <feifan.pub@gmail.com>
2015-03-09 19:54:59 -0700

    fix a package issue

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dj-sso-server',
-    version='0.2',
+    version='0.4',
     packages=['djssoserver'],
     include_package_data=True,
     license='GPL v2.0',
